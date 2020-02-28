8588e9eb65286c28ee0d963c12ad5fee11067ef7
gudian1618 <1761844211@qq.com>
2020-02-28 21:17:27 +0800

    Initial commit

---
+++ src/main/java/com/github/gudian1618/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.github.gudian1618.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gudian1618
+ * @version 1.0
+ * @date 2020/2/28 9:00 下午
+ */
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "你好SpringBoot";
+    }
+}
---
+++ src/test/java/com/github/gudian1618/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gudian1618;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
